7b655559adff3e2cc6c4f12fc9e45636b72fc6f5
mohit-1089 <mohitrathour1213@gmail.com>
2022-03-28 00:50:00 +0530

    Added SignUp Activity

--- Limiter/app/src/main/java/com/example/limiter/Login.java
+++ Limiter/app/src/main/java/com/example/limiter/Login.java
@@ -2,7 +2,9 @@ package com.example.limiter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void openSignInPage(View view) {
+
+        Intent intent=new Intent(this, SignUp.class);
+        startActivity(intent);
     }
 }
