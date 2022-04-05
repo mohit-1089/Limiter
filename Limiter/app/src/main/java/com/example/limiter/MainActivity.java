7a46c5ee0b02e1bfc5836a1a8e656fe3cbb0539f
mohit-1089 <mohitrathour1213@gmail.com>
2022-04-06 00:09:25 +0530

    Files updated

--- Limiter/app/src/main/java/com/example/limiter/MainActivity.java
+++ Limiter/app/src/main/java/com/example/limiter/MainActivity.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
+
+    Connection conn= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void startLoginActivity(View view) {
@@ -19,4 +28,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent=new Intent(this,Login.class);
         startActivity(intent);
     }
+
+
+
 }
